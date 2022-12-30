91f5aaad983a803551a19e40afe8bd9fec6e8331
Alec Santamaria <alecsantamaria@gmail.com>
2022-12-30 11:29:14 -0500

    working

--- app.js
+++ app.js
@@ -1,45 +1,85 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require("dotenv").config();
+require('dotenv/config');
 
 // ℹ️ Connects to the database
-require("./db");
+require('./db');
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
-const express = require("express");
+const express = require('express');
 
+//USE THIS FOR REACT
 const cors = require('cors');
+
+
 const app = express();
+
+//USE THIS FOR REACT
+let whitelist = ['http://localhost:3000'];
+let corsOptions = {
+    origin: (origin, callback)=>{
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },credentials: true
+}
+
+app.use(cors(corsOptions));
+
+//==========================================
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
+//==========================================
+
+let flash = require('connect-flash');
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
-const projectName = 'RentATrack';
+// default value for title local
+const projectName = 'Project3';
 const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
-
+// 👇 Start handling routes here
 
 app.use(
     session({
       secret: '123secret',
-      cookieName: '321secret',
       resave: true,
       saveUninitialized: true,
+      cookie: {
+        maxAge: 600000
+      }, // ADDED code below !!!
       store: MongoStore.create({
-        mongoUrl: process.env.MONGOURL
+        mongoUrl: 'mongodb://localhost/Project3'
       })
     })
   );
   
+  app.use(flash());
 
-// Routes
+  app.use(function (req, res, next) {
+    // im making a template variable called theUser and imequalling it to 
+    // the user object in the session
+    res.locals.theUser = req.session.currentlyLoggedIn;
+    res.locals.errorMessage = req.flash("error");
+    res.locals.successMessage = req.flash("success");
+    next();
+  })
+
+
+
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+
+// 👇 Start handling routes here
 const indexRoutes = require("./routes/index.routes");
-app.use("/", indexRoutes);
+app.use("/api", indexRoutes);
 
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
@@ -51,8 +91,12 @@ const Services = require('./routes/services.routes');
 app.use('/services', Services);
 
 
-
-// To handle errors. Routes that don't exist or errors that you handle in specific routes
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
+
+
+//USE THIS FOR REACT
+app.use('*',cors());
+
 
 module.exports = app;
--- config/index.js
+++ config/index.js
@@ -7,40 +7,26 @@ const logger = require("morgan");
 
 // ℹ️ Needed when we deal with cookies (we will when dealing with authentication)
 // https://www.npmjs.com/package/cookie-parser
+const cookieParser = require("cookie-parser");
 
-
-// ℹ️ Needed to accept from requests from 'the outside'. CORS stands for cross origin resource sharing
-// unless the request if from the same domain, by default express wont accept POST requests
+// ℹ️ Needed to accept requests from 'the outside'. CORS stands for cross origin resource sharing
+// unless the request is made from the same domain, by default express wont accept POST requests
 const cors = require("cors");
 
 const FRONTEND_URL = process.env.ORIGIN || "http://localhost:3000";
 
 // Middleware configuration
 module.exports = (app) => {
-  // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
-  // Services like heroku use something called a proxy and you need to add this to your server
+  // Because this will be hosted on a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
+  // Services like Fly use something called a proxy and you need to add this to your server
   app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
-  // app.use(
-  //   cors({
-  //     origin: [FRONTEND_URL]
-  //   })
-  // );
-
-let whitelist = [process.env.ORIGIN];
-let corsOptions = {
-    credentials: true,
-    origin: (origin, callback)=>{
-      console.log({origin, callback});
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    }
-  }
-  app.use(cors(corsOptions));
+  app.use(
+    cors({
+      origin: [FRONTEND_URL],
+    })
+  );
 
   // In development environment the app logs
   app.use(logger("dev"));
@@ -48,5 +34,5 @@ let corsOptions = {
   // To have access to `body` property in the request
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
-  
+  app.use(cookieParser());
 };
